aa121592f124890e5ed50f10f6a87827b50d12b0
wosiwq <z_weipeng@126.com>
2024-01-18 17:23:05 +0800

    Add @nplayer/vue and nplayer dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import '@/api/instance'
+import '@unocss/reset/tailwind-compat.css'
+import 'virtual:uno.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import { createPinia } from 'pinia'
-import 'virtual:uno.css'
-import '@unocss/reset/tailwind-compat.css'
 import { createApp } from 'vue'
+import NPlayer from '@nplayer/vue'
 
 import App from './App.vue'
 import router from './router'
@@ -12,5 +13,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(NPlayer)
 
 app.mount('#app')
