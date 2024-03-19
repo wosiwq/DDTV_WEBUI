33c5ef3335b43ac5dd8345e37394a4efd3ee0fea
wosiwq <z_weipeng@126.com>
2024-03-19 15:53:44 +0800

    更改为使用vue-virtual-scroller实现虚拟滚动

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import '@/api/instance'
 import '@unocss/reset/tailwind-compat.css'
 import 'virtual:uno.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
+import { RecycleScroller } from 'vue-virtual-scroller'
 
 import App from './App.vue'
 import router from './router'
@@ -12,5 +14,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.component('RecycleScroller', RecycleScroller)
 
 app.mount('#app')
--- src/views/RoomList.vue
+++ src/views/RoomList.vue
@@ -2,7 +2,7 @@
   <meta name="referrer" content="no-referrer" />
   <div class="h-full flex justify-between">
     <div class="h-full w-50% flex items-center justify-center">
-      <ElCard class="h-90% w-90%" v-loading="isLoading" ref="tableDiv" style="--el-card-padding: 0">
+      <ElCard class="h-90% w-90%" v-loading="isLoading" ref="cardDiv" style="--el-card-padding: 0">
         <template #header>
           <div ref="headerDiv" class="flex justify-between p-1 pl-2">
             <ElCheckbox
@@ -16,35 +16,29 @@
               placeholder="用户名/UID"></ElInput>
           </div>
         </template>
-        <div
-          v-bind="containerProps"
-          :style="'height:' + (tableDivSize.height.value - headerDivSize.height.value) + 'px'">
-          <div v-bind="wrapperProps">
-            <div
-              class="flex items-center border-t-1 pl-2 pr-1 dark:border-[#414243]"
-              v-for="item in list"
-              :key="item.data.uid.toString()">
-              <ElCheckbox
-                v-model="item.data.checked"
-                @change="(checked) => handleCheckboxChange(item.data, checked)"></ElCheckbox>
-              <RoomUser
-                class="w-full"
-                :room-info="item.data.roomInfo"
-                :user-info="item.data.userInfo"
-                :is-download="item.data.taskStatus.isDownload">
-                <div class="flex items-center space-x-1">
-                  <ElTag :type="item.data.userInfo.isAutoRec ? 'success' : 'danger'">
-                    自动录制
-                  </ElTag>
-                  <ElTag :type="item.data.userInfo.isRecDanmu ? 'success' : 'danger'">
-                    弹幕录制
-                  </ElTag>
-                  <ElTag :type="item.data.userInfo.isRemind ? 'success' : 'danger'">直播提醒</ElTag>
-                </div>
-              </RoomUser>
-            </div>
+        <RecycleScroller
+          :items="roomInfoList"
+          :item-size="69"
+          key-field="uid"
+          v-slot="{ item }"
+          :style="'height:' + (tableDivSize.height.value - headerDivSize.height.value - 10) + 'px'">
+          <div class="flex items-center border-t-1 pl-2 pr-1 dark:border-[#414243]">
+            <ElCheckbox
+              v-model="item.checked"
+              @change="(checked) => handleCheckboxChange(item, checked)"></ElCheckbox>
+            <RoomUser
+              class="w-full"
+              :room-info="item.roomInfo"
+              :user-info="item.userInfo"
+              :is-download="item.taskStatus.isDownload">
+              <div class="flex items-center space-x-1">
+                <ElTag :type="item.userInfo.isAutoRec ? 'success' : 'danger'">自动录制</ElTag>
+                <ElTag :type="item.userInfo.isRecDanmu ? 'success' : 'danger'">弹幕录制</ElTag>
+                <ElTag :type="item.userInfo.isRemind ? 'success' : 'danger'">直播提醒</ElTag>
+              </div>
+            </RoomUser>
           </div>
-        </div>
+        </RecycleScroller>
       </ElCard>
     </div>
     <div class="h-full w-50% flex items-center justify-center">
@@ -60,7 +54,7 @@
 <script lang="tsx" setup>
 import type { CompleteInfoWithCheck } from '@/types/response'
 import type { CheckboxValueType } from 'element-plus'
-import { useElementSize, useVirtualList } from '@vueuse/core'
+import { useElementSize } from '@vueuse/core'
 import { getDetailedRoomInfoList } from '@/api/get_room'
 import { delRoom } from '@/api/set_room'
 import { SearchType } from '@/enums'
@@ -78,9 +72,6 @@ const roomInfoList = computed(() => {
     )
   })
 })
-const { list, containerProps, wrapperProps } = useVirtualList(roomInfoList, {
-  itemHeight: 69
-})
 const allSelected = computed(() => {
   return roomInfoList.value.every((item) => item.checked)
 })
@@ -89,9 +80,9 @@ const containsChecked = computed(() => {
 })
 const total = ref(0)
 const isLoading = ref(true)
-const tableDiv = ref<HTMLElement | null>(null)
+const cardDiv = ref<HTMLElement | null>(null)
 const headerDiv = ref<HTMLElement | null>(null)
-const tableDivSize = useElementSize(tableDiv)
+const tableDivSize = useElementSize(cardDiv)
 const headerDivSize = useElementSize(headerDiv)
 
 const getData = () => {
